7ef10d290ba4af7fe565bab81fa3a60fc83c26c1
LudovicComtois <78489934+LudovicComtois@users.noreply.github.com>
2023-04-05 15:03:29 -0400

    interface prog suite
    ajout d'interactions entre MQTT et l'interface

--- Puissance Gant/Programmation/Code_Interface/Programme_IntergacePuissanceGant.py
+++ Puissance Gant/Programmation/Code_Interface/Programme_IntergacePuissanceGant.py
@@ -1,4 +1,6 @@
-import sys, os
+import os
+import sys
+import pyqtgraph
 from PyQt5 import QtWidgets
 
 # Transfromer l'interface de Qt Designer (format .ui) en code python directement
@@ -7,22 +9,67 @@ os.system("pyuic5 InterfacePuissanceGant.ui -o InterfacePuissanceGant.py")
 
 # import pyqtgraph as pygraph
 from InterfacePuissanceGant import Ui_MainWindow
+from commInterface import CommInterface
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.setupPlot()
+
+        self.commInterface = CommInterface()
+        self.commInterface.start()
+        self.commInterface.msgResist.connect(text_CommandeMoteurs)
+        self.commInterface.msgConnexion.connect(etatConnexionServeur)
+        self.commInterface.msgDelaiResist.connect(label_delaiResist)
+        self.commInterface.valEnergie.connect(plot_Energie)
+
         self.Label_CrochetVert.setVisible(False)
 
         self.LineEdit_ConsoleManuel.returnPressed.connect(consoleManuel_EnvoyerCommande)
         self.Button_ArretUrgence.clicked.connect(button_ArretUrgence)
         self.Button_ModeManuel.clicked.connect(button_ModeManuel)
-        self.Button_DemarrageServeur.clicked.connect(button_DemarrageServeur)
+        #self.Button_DemarrageServeur.clicked.connect(button_DemarrageServeur)
+
+    def setupPlot(self):
+        tailleValEnergieTableau = 20
+        self.valEnergieTableau = tailleValEnergieTableau * [0]
+        self.Plot_Energie.setYRange(0, 600)
+
+
+# Afficher la commande actuelle des moteurs dans le tableau de l'interface
+def text_CommandeMoteurs(msg):
+    msgRecu = ""
+    msgDemarre = False
+    for c in msg:
+        if msgDemarre:
+            match c:
+                case 'A':
+                    window.Text_PourcentagePouce.setText(msgRecu + '%')
+                    msgRecu = ""
+                case 'B':
+                    window.Text_PourcentageIndex.setText(msgRecu + '%')
+                    msgRecu = ""
+                case 'C':
+                    window.Text_PourcentageMajeur.setText(msgRecu + '%')
+                    msgRecu = ""
+                case 'D':
+                    window.Text_PourcentageInclinaison.setText(msgRecu + '%')
+                    msgRecu = ""
+                case 'E': # à retirer quand la comm n'aura plus de E
+                    #window.Text_PourcentageRotation.setText(msgRecu + '%')
+                    msgRecu = ""
+                case '>':
+                    msgRecu = ""
+                case _:
+                    msgRecu = msgRecu + c
+        elif c == '<':
+            msgDemarre = True
 
 
 # region code de boutons
-def button_ModeManuel(self):
+def button_ModeManuel():
     # ===================
     # Ajouter le changement de vitesse pour avoir tous les moteurs à 50%
     # Possiblement fait avec un tableau de valeurs de position des moteurs envoyé à chaque
@@ -41,7 +88,7 @@ def button_ModeManuel(self):
         window.LineEdit_ConsoleManuel.setPlaceholderText("Mode automatique activé, console bloquée")
 
 
-def button_ArretUrgence(self):
+def button_ArretUrgence():
     if window.Button_ArretUrgence.isChecked():
         window.Text_EtatDuRobot.setText("Arrêt d'urgence activé." + "\n\n" + "Robot désactivé, moteurs bloqués.")
         window.Button_ModeManuel.setEnabled(False)
@@ -53,15 +100,11 @@ def button_ArretUrgence(self):
         window.Button_ModeManuel.setChecked(True)
 
 
-def button_DemarrageServeur():
-    # inclure l'état de connexion du serveur. Actuellement fait avec le label du crochet
-    # Ajouter la logique de connexion du serveur
-    if window.Label_CroixRouge.isVisible():
-        window.Label_CroixRouge.setVisible(False)
-        window.Label_CrochetVert.setVisible(True)
-    else:
-        window.Label_CroixRouge.setVisible(True)
-        window.Label_CrochetVert.setVisible(False)
+def plot_Energie(val):
+    window.valEnergieTableau.pop(0)
+    window.valEnergieTableau.append(val)
+    window.Plot_Energie.clear()
+    window.Plot_Energie.plot(window.valEnergieTableau)
 
 # endregion
 
@@ -80,9 +123,25 @@ def consoleManuel_EnvoyerCommande():
     window.Text_EtatDuRobot.setText(texte)
     window.LineEdit_ConsoleManuel.clear()
 
+
+def label_delaiResist(msg):
+    window.Label_DelaiResist.setText(msg)
+
 # endregion
 
 # region code pour l'accès à partir du code de communication
+
+# Indique si l'interface est connectée au serveur MQTT.
+def etatConnexionServeur(msg: bool):
+    if msg:
+        window.Label_CroixRouge.setVisible(False)
+        window.Label_CrochetVert.setVisible(True)
+    else:
+        window.Label_CroixRouge.setVisible(True)
+        window.Label_CrochetVert.setVisible(False)
+
+# endregion
+# region Code autre
 
 # endregion
 
--- Puissance Gant/Programmation/Code_Interface/commInterface.py
+++ Puissance Gant/Programmation/Code_Interface/commInterface.py
@@ -1,42 +1,81 @@
+from PyQt5.QtCore import QThread, pyqtSignal
 import paho.mqtt.client as mqtt
-
-MQTT_ADDRESS = "192.168.137.254"
-MQTT_USER = 'puissance'
-MQTT_PASSWORD = 'puissance'
-MQTT_TOPIC_RESIST = 'Eq7_PuissanceGant_S4/gant/resistance'
-MQTT_TOPIC_CALIB = 'Eq7_PuissanceGant_S4/gant/reset'
+import time
 
 
-# ----------------------------------------------------------------------------
-# Fonction MQTT 
+class CommInterface(QThread):
+    MQTT_ADDRESS = "192.168.137.254"
+    MQTT_USER = 'puissance'
+    MQTT_PASSWORD = 'puissance'
+    MQTT_TOPIC_RESIST = 'Eq7_PuissanceGant_S4/gant/resistance'
+    MQTT_TOPIC_CALIB = 'Eq7_PuissanceGant_S4/gant/reset'
 
-def on_connect(client, userdata, flags, rc):
-    print('Connected with result code ' + str(rc))
-    client.subscribe(MQTT_TOPIC_RESIST, 1)
-    client.subscribe(MQTT_TOPIC_CALIB, 1)
+    msgResist = pyqtSignal(str)
+    msgDelaiResist = pyqtSignal(str)
+    delaiResist = time.time()
+    msgConnexion = pyqtSignal(bool)
+    valEnergie = pyqtSignal(int)
+
+    #tailleValEnergieTableau = 20;
+    #valEnergieTableau = tailleValEnergieTableau*[0]
+
+    # ----------------------------------------------------------------------------
+    # Fonction MQTT
+    def __init__(self):
+        QThread.__init__(self)
+        print('MQTT to influxDB bridge')
+
+        global mqtt_client
+
+        mqtt_client = mqtt.Client("interface")
+        mqtt_client.enable_logger()
+        mqtt_client.username_pw_set(self.MQTT_USER, self.MQTT_PASSWORD)
+        mqtt_client.on_connect = self.on_connect
+        mqtt_client.on_disconnect = self.on_disconnect
+        mqtt_client.on_message = self.on_message
 
 
-def on_message(client, userdata, msg):
-    # print(str(msg.topic))
-    mqtt_client.publish('Eq7_PuissanceGant_S4/OpenCR/test', str("BENIS"), qos=0, retain=False)
+    def run(self):
+        mqtt_client.connect_async(host=self.MQTT_ADDRESS, port=1883)
+        mqtt_client.loop_forever()
+
+    def kill(self):
+        mqtt_client.disconnect()
+        self.terminate()
+
+    def on_connect(self, client, userdata, flags, rc):
+        print('Connected with result code ' + str(rc))
+        client.subscribe(self.MQTT_TOPIC_RESIST, 1)
+        client.subscribe(self.MQTT_TOPIC_CALIB, 1)
+
+        self.msgConnexion.emit(True)
+
+    def on_disconnect(self, client, userdata, flags):
+        print('Disconnected from broker')
+        self.msgConnexion.emit(False)
 
 
-# ----------------------------------------------------------------------------
-# main 
 
-def main():
-    global mqtt_client
+    def on_message(self, client, userdata, msg):
+        # print(str(msg.topic))
+        if msg.topic == self.MQTT_TOPIC_RESIST:  # Message contenant la commande de l'ESP32
+            self.msgResist.emit(str(msg.payload))
+            delai = f"{1000*(time.time() - self.delaiResist):.1f}"
+            self.msgDelaiResist.emit(delai + " ms")
 
-    mqtt_client = mqtt.Client("pc")
-    mqtt_client.enable_logger()
-    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
-
-    mqtt_client.connect_async(host=MQTT_ADDRESS, port=1883)
-    mqtt_client.loop_forever()
+            #self.valEnergieTableau.append(int(1000*(time.time() - self.delaiResist)))
+            #self.valEnergieTableau.pop(0)
+            self.valEnergie.emit(int(1000*(time.time() - self.delaiResist)))
+            self.delaiResist = time.time()
+        mqtt_client.publish('Eq7_PuissanceGant_S4/OpenCR/test', str("BENIS"), qos=0, retain=False)
 
 
-if __name__ == '__main__':
-    print('MQTT to influxDB bridge')
-    main()
+    # ----------------------------------------------------------------------------
+    # main
+
+
+
+
+#    if __name__ == '__main__':
+#        print('MQTT to influxDB bridge')
+#        main()
